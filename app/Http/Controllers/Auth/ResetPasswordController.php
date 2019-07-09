e950f8975a7d217252f157c5e3b59ff1c5277ccd
abw1794guy <abw1794guy@gmail.com>
2019-07-09 19:33:10 +0000

    找回密碼成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
-
+use Illuminate\Http\Request;
 class ResetPasswordController extends Controller
 {
     /*
@@ -35,5 +35,10 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', '密碼更新成功，您已重新登入！');
+        return redirect($this->redirectPath());
     }
 }
