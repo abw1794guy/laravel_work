26355a0df1f2bce0668be9a3e3a9c45c2e5915f9
abw1794guy <abw1794guy@gmail.com>
2019-07-10 11:08:34 +0000

    用戶頁面初版

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,22 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
-// 用户身份验证相关的路由
+// 用户身份驗證
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-// 用户注册相关路由
+// 用户註冊
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 
-// 密码重置相关路由
+// 密碼重置
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
-// Email 认证相关路由
+// Email 認證
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
@@ -39,3 +39,6 @@ Route::get('/', 'PagesController@root')->name('root');
 // Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+//  個人資料路由
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
