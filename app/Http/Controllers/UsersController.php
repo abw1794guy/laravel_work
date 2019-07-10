6adb17750b3c8ef0f59f5e80b4c3f69d2a43216d
abw1794guy <abw1794guy@gmail.com>
2019-07-10 17:13:33 +0000

    訪問授權

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,23 +14,31 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
     //編輯資料
-    public function edit(User $user)
+public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
-    //更新個人資料
+    // 更新
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
-                $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
         }
 
         $user->update($data);
-        return redirect()->route('users.show', $user->id)->with('success', '個人資料更新成功！');
-}
+        return redirect()->route('users.show', $user->id)->with('success', '資料更新成功！');
+    }
+
+     public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
 }
