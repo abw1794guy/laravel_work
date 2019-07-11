92e3dd4469a964937953d1d21644e4f7372ef10a
abw1794guy <abw1794guy@gmail.com>
2019-07-11 19:59:57 +0000

    消息通知列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -21,9 +21,9 @@
 
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
-        <!-- Authentication Links -->
+<!-- Authentication Links -->
         @guest
-          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登入</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登錄</a></li>
           <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">註冊</a></li>
         @else
           <li class="nav-item">
@@ -31,7 +31,11 @@
               <i class="fa fa-plus"></i>
             </a>
           </li>
-
+          <li class="nav-item notification-badge">
+            <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
+              {{ Auth::user()->notification_count }}
+            </a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <img src="https://about.yunchieh.cloud/images/avatar.jpg" class="img-responsive img-circle" width="30px" height="30px">
