5f1716d76999c33369e1f5acaeed7a072de95e31
abw1794guy <abw1794guy@gmail.com>
2019-07-11 19:46:35 +0000

    回覆消息通知

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,29 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
-
+use Auth;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable, MustVerifyEmailTrait;
+
+    use MustVerifyEmailTrait;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+    public function notify($instance)
+    {
+        // 如果要通知的人是當前用戶，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        // 只有數據庫類型通知才需提醒，直接發送 Email 或者其他的都 Pass
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
 
     protected $fillable = [
         'name', 'email', 'password','introduction','avatar'
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -2,17 +2,17 @@
 
 namespace App\Observers;
 
+use App\Notifications\TopicReplied;
 use App\Models\Reply;
-
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
 class ReplyObserver
 {
     public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知話題作者有新的評論
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
     public function creating(Reply $reply)
     {
