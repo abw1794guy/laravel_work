bbaab589d59f001fe0a843179c14eb86b62cfc68
abw1794guy <abw1794guy@gmail.com>
2019-07-12 14:44:39 +0000

    設定安裝administrator

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Spatie\Permission\Traits\HasRoles;
+
 use Auth;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -7,43 +7,49 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        // 獲取 Faker 實例
+        // 获取 Faker 实例
         $faker = app(Faker\Generator::class);
 
-        // 頭像假數據
+        // 头像假数据
         $avatars = [
-            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
         ];
 
-        // 生成數據集合
+        // 生成数据集合
         $users = factory(User::class)
                         ->times(10)
                         ->make()
                         ->each(function ($user, $index)
                             use ($faker, $avatars)
         {
-            // 從頭像數組中隨機取出一個並賦值
+            // 从头像数组中随机取出一个并赋值
             $user->avatar = $faker->randomElement($avatars);
         });
 
-        // 讓隱藏字段可見，並將數據集合轉換為數組
+        // 让隐藏字段可见，并将数据集合转换为数组
         $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
 
-        // 插入到數據庫中
+        // 插入到数据库中
         User::insert($user_array);
 
-        // 初始化用戶角色，將 1 號用戶指派為『站長』
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summer@example.com';
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
         $user->assignRole('Founder');
 
-        // 將 2 號用戶指派為『管理員』
+        // 将 2 号用户指派为『管理员』
         $user = User::find(2);
         $user->assignRole('Maintainer');
 
     }
-
 }
